232b99ad364f5dea9c0d4d07ab7211fca7cb2d9e
faroukbmiled <fcfaroukfc@gmail.com>
2022-04-13 23:06:15 +0100

    header

--- main.h
+++ main.h
@@ -17,9 +17,7 @@ void prompt(void);
 void signal_handler(int signo);
 char *read_line(void);
 int execute(char **token);
-void display_env(void);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
-char *_getenv(const char *name);
 
 #endif /*_MAIN_H_*/
