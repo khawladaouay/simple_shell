f325b7940d19fd84b20df73752d8e0aaeaab4133
faroukbmiled <fcfaroukfc@gmail.com>
2022-04-13 22:31:46 +0100

    fixes

--- func.c
+++ func.c
@@ -37,38 +37,3 @@ int _strcmp(char *s1, char *s2)
 		return (*s1 - *s2);
 	}
 }
-
-/**
- * _getenv - gets value of env
- * @name: name of variable in env
- *
- * Return: NULL or value of env
- */
-char *_getenv(const char *name)
-{
-	char **envPtr;
-	char *charPtr;
-	const char *nPtr;
-
-	for (envPtr = environ; *envPtr != NULL; envPtr++)
-	{
-		for (charPtr = *envPtr, nPtr = name; *charPtr == *nPtr; charPtr++, nPtr++)
-		{
-			if (*charPtr == '=')
-				break;
-		}
-		if ((*charPtr == '=') && (*nPtr == '\0'))
-			return (charPtr + 1);
-	}
-	return (NULL);
-}
-
-/**
- * av_error - prints perror with av[0]
- * @av: argument vector
- * Return: VOID
- */
-void av_error(char **av)
-{
-	perror(av[0]);
-}
--- test.c
+++ test.c
@@ -19,20 +19,3 @@ char **_strtok(char *line)
 	token[i] = NULL;
 	return (token);
 }
-
-/**
- * display_env - displays env
- *
- * Return: VOID
- */
-void display_env(void)
-{
-	unsigned int i = 0;
-
-	while (environ[i])
-	{
-		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
-	}
-}
